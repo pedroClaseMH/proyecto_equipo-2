3bb191dae35780995db2751d912feb6a800c7365
Juanca <jvallejor@educantabria.es>
2026-01-27 17:33:42 +0100

    Añado mis casos de prueba

---
+++ docs/Juancamilo/yepa.java
@@ -0,0 +1,20 @@
+package trabajochavales;
+
+public class yepa {
+    
+    public static void main(String[] args) {
+     int a = 3;
+     int b = -5;
+     
+     int c = suma(a,b);
+     
+        System.out.println("La suma es = "+ c);
+        
+    }        
+        public static int suma(int a, int b){
+            return a + b;
+        }
+        
+    }
+    
+
