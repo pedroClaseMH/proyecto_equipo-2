a4a28e1bf0fd6f1e809ea692780c33e1c919c391
Ossel18 <osantamariab01@educantabria.es>
2026-01-27 17:14:18 +0100

    Casos de prueba de Ossel

---
+++ docs/Ossel/Main.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.holaclase;
+
+/**
+ *
+ * @author DAW120
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 8;
+        
+        int e = suma(a, b);
+        
+        System.out.println("La suma es: " + e);
+    }
+    
+    public static int suma(int a, int b){
+        return a + b;
+    }
+}
