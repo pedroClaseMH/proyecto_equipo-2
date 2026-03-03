0b68a2352ebf0018bf068fdfe326a030fcb42b06
Chisca <dchiscap2501@educantabria.es>
2026-03-03 16:01:30 +0100

    DavidChisca-#20-subida_archivo_java
    He subido un archivo java en mi carpeta personal

---
+++ docs/Chisca/Suma_enteros.java
@@ -0,0 +1,21 @@
+
+package Entornos;
+
+
+public class Suma_enteros {
+
+    public static void main(String[] args) {
+
+
+        int numero1 = 10;
+        int numero2 = 20;
+
+
+        int suma = numero1 + numero2;
+
+
+        System.out.println("El primer número es: " + numero1);
+        System.out.println("El segundo número es: " + numero2);
+        System.out.println("La suma es: " + suma);
+    }
+}
